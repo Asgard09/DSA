0d3abdc83f76e1c2bdd83ddf8274d13c1c5e4669
Asgard09 <thanhphuc04nh@gmail.com>
2025-01-08 08:45:57 +0700

    Valid Palindrome II

--- 1. Array_and_String/_680_Valid_Palindrome_II.java
+++ 1. Array_and_String/_680_Valid_Palindrome_II.java
@@ -1,12 +1,23 @@
 public class _680_Valid_Palindrome_II {
     public static boolean validPalindrome(String s) {
-        int j = s.length() -1 ;
-        for (int i = 0; i <= j; i++) {
-            if (s.charAt(i) != s.charAt(j) && i!=j-1) {
-                return false;
-            }else if (i == j - 1) {
-                return true;
+        int j = s.length()-1;
+        int i = 0;
+        while(i<=j){
+            if(s.charAt(i) != s.charAt(j)){
+                return isValid(s,i+1,j) || isValid(s,i,j-1);
             }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static boolean isValid(String s, int i, int j){
+        while(i<=j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
             j--;
         }
         return true;
