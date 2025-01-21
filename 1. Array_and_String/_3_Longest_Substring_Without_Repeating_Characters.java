cd8962d1d8c37515eba7f06715d127ee4e0907a8
Asgard09 <thanhphuc04nh@gmail.com>
2025-01-21 17:12:19 +0700

    update sliding window

--- 1. Array_and_String/_3_Longest_Substring_Without_Repeating_Characters.java
+++ 1. Array_and_String/_3_Longest_Substring_Without_Repeating_Characters.java
@@ -1,22 +1,22 @@
 import java.util.HashSet;
+import java.util.Set;
 
 public class _3_Longest_Substring_Without_Repeating_Characters {
     public static int lengthOfLongestSubstring(String s) {
         int left = 0;
-        int maxLength = 0;
-        HashSet<Character> charSet = new HashSet<>();
-
-        for (int right = 0; right < s.length(); right++) {
+        int longest = 0;
+        Set<Character> charSet = new HashSet<>();  
+        for(int right = 0; right<s.length();right++ ){
+            // ensure in the set dont have any character like 's.charAt(right)'
+            // left will be increase correct
             while (charSet.contains(s.charAt(right))) {
                 charSet.remove(s.charAt(left));
                 left++;
             }
-
             charSet.add(s.charAt(right));
-            maxLength = Math.max(maxLength, right - left + 1);
+            longest = Math.max(longest, right - left + 1);
         }
-
-        return maxLength;       
+        return longest;
     }
 
     public static void main(String[] args) {
