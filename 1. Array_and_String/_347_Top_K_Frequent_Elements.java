27cb3511fe5f506a17119ebc63b87b18786d7131
Asgard09 <thanhphuc04nh@gmail.com>
2025-01-27 19:24:57 +0700

    Top K Frequent Elements

---
+++ 1. Array_and_String/_347_Top_K_Frequent_Elements.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class _347_Top_K_Frequent_Elements {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer,Integer> tem=new HashMap<>();
+        for(int i:nums){
+            tem.put(i,tem.getOrDefault(i,0)+1);
+        }
+        PriorityQueue<Map.Entry<Integer,Integer>> pq=new PriorityQueue<>(
+            (a,b)->b.getValue()-a.getValue()
+        );
+        for(Map.Entry<Integer,Integer> e:tem.entrySet()){
+            pq.offer(e);
+        }
+        int[] res=new int[k];
+        for(int i=0;i<k;i++){
+            res[i]=pq.poll().getKey();
+        }
+        return res;
+    }
+}
--- 1. Array_and_String/_424_Longest_Repeating_Character_Replacement.java
+++ 1. Array_and_String/_424_Longest_Repeating_Character_Replacement.java
@@ -1,5 +1,2 @@
 public class _424_Longest_Repeating_Character_Replacement {
-    public int characterReplacement(String s, int k) {
-        
-    }
 }
