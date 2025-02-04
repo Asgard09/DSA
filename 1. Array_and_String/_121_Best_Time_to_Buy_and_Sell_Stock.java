425de4c6bca03ad7655e94c56d77d2f95af15502
Asgard09 <thanhphuc04nh@gmail.com>
2025-02-04 09:17:59 +0700

    Sliding Window + Hash Map

--- 1. Array_and_String/_121_Best_Time_to_Buy_and_Sell_Stock.java
+++ 1. Array_and_String/_121_Best_Time_to_Buy_and_Sell_Stock.java
@@ -1,15 +1,18 @@
 public class _121_Best_Time_to_Buy_and_Sell_Stock {
     public static int maxProfit(int[] prices) {
-        int buy = prices[0];
-        int profit = 0;
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] < buy) {
-                buy = prices[i];
-            } else if (prices[i] - buy > profit) {
-                profit = prices[i] - buy;
+        int n = prices.length;
+        int profit = 0; //window
+        int buy = 0; //left
+
+        for (int sell = 1 /*right */; sell < n; sell++) {
+            if (prices[sell] < prices[buy]) {
+                prices[buy] = prices[sell];
             }
+            profit = Math.max(profit, prices[sell] - prices[buy]);
         }
+
         return profit;
+
     }
 
     public static void main(String[] args) {
