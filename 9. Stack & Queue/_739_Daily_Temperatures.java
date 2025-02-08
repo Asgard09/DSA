27571ff63de9bea408366c65a6b7538304ddf205
Asgard09 <thanhphuc04nh@gmail.com>
2025-02-08 14:09:17 +0700

    update

--- 9. Stack & Queue/_739_Daily_Temperatures.java
+++ 9. Stack & Queue/_739_Daily_Temperatures.java
@@ -1,18 +1,23 @@
 import java.util.Stack;
 
 public class _739_Daily_Temperatures {
-    public int[] dailyTemperatures(int[] temperatures) {
+    public static int[] dailyTemperatures(int[] temperatures) {
         Stack<Integer> stack = new Stack<>();
         int[] result = new int[temperatures.length];
 
-        for (int i = 0; i < temperatures.length; i++) {
-            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
-                int idx = stack.pop();
-                result[idx] = i - idx;
+        for(int i = 0; i<temperatures.length; i++){
+            while (!stack.isEmpty() && temperatures[i]<temperatures[stack.peek()]) {
+                int index = stack.pop();
+                result[i] = i - index;
             }
             stack.push(i);
         }
 
         return result;  
-    }   
+    }  
+    
+    public static void main(String[] args) {
+        int[] temperatures = {73,74,75,71,69,72,76,73};
+        System.out.println(dailyTemperatures(temperatures));
+    }
 }
