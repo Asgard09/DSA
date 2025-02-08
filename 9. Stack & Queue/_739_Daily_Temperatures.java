f4498fcf3b02786d739f3ff2472e9013585e7a1b
Asgard09 <thanhphuc04nh@gmail.com>
2025-02-08 10:11:01 +0700

    daily temperature

---
+++ 9. Stack & Queue/_739_Daily_Temperatures.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+
+public class _739_Daily_Temperatures {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[temperatures.length];
+
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int idx = stack.pop();
+                result[idx] = i - idx;
+            }
+            stack.push(i);
+        }
+
+        return result;  
+    }   
+}
