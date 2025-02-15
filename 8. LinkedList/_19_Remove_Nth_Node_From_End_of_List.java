67caaeb8b72aa0ba35f7f9b835344624cab40258
Asgard09 <thanhphuc04nh@gmail.com>
2025-02-15 14:56:25 +0700

    Dummy Node

---
+++ 8. LinkedList/_19_Remove_Nth_Node_From_End_of_List.java
@@ -0,0 +1,19 @@
+public class _19_Remove_Nth_Node_From_End_of_List {
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode res = new ListNode(0,head);
+        ListNode dummy = res;
+
+        for (int i = 0; i < n; i++) {
+            head = head.next;
+        }
+
+        while (head != null) {
+            head = head.next;
+            dummy = dummy.next;
+        }
+
+        dummy.next = dummy.next.next;
+
+        return res.next;
+    }
+}
