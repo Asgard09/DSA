6cab532638ca6838e13b8f10b2e2239f9414729a
Asgard09 <thanhphuc04nh@gmail.com>
2025-02-27 10:57:36 +0700

    Update

--- 11. Tree/_572_Subtree_of_Another_Tree.java
+++ 11. Tree/_572_Subtree_of_Another_Tree.java
@@ -1,20 +1,16 @@
 public class _572_Subtree_of_Another_Tree {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) return false; 
+        if (isIdentical(root, subRoot)) return true; 
 
-        if (root == null) return false;
-
-        /*Case 1: root and subroot have same length */
-        if (isIdentical(root, subRoot)) return true;
-
-        /*Case 2: subroot is a descendant of root */
-        return isIdentical(root.left, subRoot) || isIdentical(root.right, subRoot);
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
 
-    public boolean isIdentical(TreeNode root, TreeNode subRoot){
-        if (root == null && subRoot == null) return true;
-
-        if (root == null || subRoot == null || root.val != subRoot.val) return false;
-
+    public boolean isIdentical(TreeNode root, TreeNode subRoot) {
+        if (root == null && subRoot == null) return true;  
+        if (root == null || subRoot == null) return false; 
+        if (root.val != subRoot.val) return false; 
+        
         return isIdentical(root.left, subRoot.left) && isIdentical(root.right, subRoot.right);
     }
 }
