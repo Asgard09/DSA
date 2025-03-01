c1e24eeb2fe3cf0ebf36e572bc6bcc3ed1144144
Asgard09 <thanhphuc04nh@gmail.com>
2025-03-01 09:34:31 +0700

    Update

--- 11. Tree/_543_Diameter_of_Binary_Tree.java
+++ 11. Tree/_543_Diameter_of_Binary_Tree.java
@@ -1,19 +1,29 @@
+/*
+Use recursion to find:
+    The longest path through the root (calculateLength(root.left) + calculateLength(root.right)).
+    The diameter of the left subtree.
+    The diameter of the right subtree.
+Return the maximum of these three values.
+ */
 public class _543_Diameter_of_Binary_Tree {
-    int res = 0;
+    private int diameter = 0;
+
     public int diameterOfBinaryTree(TreeNode root) {
-        dfs(root);
-        return res;
+        calculateDepth(root);
+        return diameter;
     }
 
-    public int dfs(TreeNode root){
+    public int calculateDepth(TreeNode root){
         if (root == null) {
             return 0;
         }
 
-        int left = dfs(root.left);
-        int right = dfs(root.right);
-        res = Math.max(res, left+right);
+        int leftDepth = calculateDepth(root.left);
+        int rightDepth = calculateDepth(root.right);
 
-        return 1 + Math.max(left, right);
+        diameter = Math.max(diameter, leftDepth + rightDepth);
+
+        // Return the depth of the current node
+        return 1 + Math.max(leftDepth, rightDepth);
     }
 }
