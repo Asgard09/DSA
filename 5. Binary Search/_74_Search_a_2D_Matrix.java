f1d4396295aa1b6f96ebd0ad5aba6bb9d58ce6a0
Asgard09 <thanhphuc04nh@gmail.com>
2025-03-15 09:56:54 +0700

    Update

--- 5. Binary Search/_74_Search_a_2D_Matrix.java
+++ 5. Binary Search/_74_Search_a_2D_Matrix.java
@@ -1,42 +1,43 @@
 public class _74_Search_a_2D_Matrix {
     public static boolean searchMatrix(int[][] matrix, int target) {
-        int lengthOfCol = matrix[0].length;
-        int lenghtOfRow = matrix.length;
+        
+        int colLength = matrix.length;
+        int rowLength = matrix[0].length;
+        
 
-        for (int indexOfRow = 0; indexOfRow < lenghtOfRow; indexOfRow++) {
-            if (matrix[indexOfRow][lengthOfCol - 1] >= target) {
-                int temp = BinarySearch(0, lengthOfCol, matrix[indexOfRow], target);
-                if (temp == -1) continue;
-                if (matrix[indexOfRow][temp] == target ) {
-                    return true;
-                }
+        for(int col = 0; col< colLength; col++){
+            if(target >= matrix[col][0] && target <= matrix[col][rowLength -1] ){
+                return binarySearch(matrix[col], target);
+            }else{
+                continue;
             }
         }
 
         return false;
     }
 
-    public static int BinarySearch(int left, int right, int[] nums, int target){
+    public static boolean binarySearch(int[] nums, int target){
+        int left = 0;
+        int right = nums.length - 1;
         int mid = 0;
+
         while(left<=right){
-            mid = (right+left)/2;
-            
-            if (nums[mid] == target) {
-                return mid;
+            mid = (left + right)/2;
+            if(nums[mid] == target){
+                return true;
             }else if(target > nums[mid]){
                 left = mid + 1;
             }else{
                 right = mid - 1;
             }
-
         }
 
-        return -1;
+        return false;
     }
 
     public static void main(String[] args) {
-        int[][] matrix = {{1,2,4,8},{10,11,12,13},{14,20,30,40}}; 
-        int target = 10;
+        int[][] matrix = {{1}}; 
+        int target = 1;
         System.out.println(searchMatrix(matrix, target));
     }
 }
