28c5bb3a46ebe7d7bdfa2e4edb011e25f9d29cb9
Asgard09 <thanhphuc04nh@gmail.com>
2025-03-24 15:33:29 +0700

    Review

--- 1. Array_and_String/_15_3Sum.java
+++ 1. Array_and_String/_15_3Sum.java
@@ -3,34 +3,34 @@ import java.util.Arrays;
 import java.util.List;
 
 public class _15_3Sum {
-    public List<List<Integer>> threeSum(int[] nums) {       
+    public static List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length; i++) {
-            if (i > 0 && nums[i] == nums[i-1]) {
-                continue;
-            }            
+
+        for(int i = 0; i<nums.length; i++){
+            int target = -nums[i];
             int j = i + 1;
             int k = nums.length - 1;
 
-            while (j < k) {
-                int total = nums[i] + nums[j] + nums[k];
-
-                if (total > 0) {
+            while(j<k){
+                int sum = nums[j] + nums[k];
+                if(sum < target){
+                    j++;
+                }else if(sum > target){
                     k--;
-                } else if (total < 0) {
-                    j++;
-                } else {
+                }else{
                     res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
-
-                    while (nums[j] == nums[j-1] && j < k) {
-                        j++;
-                    }
+                    continue;
                 }
             }
         }
+
         return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1,0,1,2,-1,-4};
+        System.out.println(threeSum(nums).toString());
     }
 
 }
