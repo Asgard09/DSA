168430123af35ff6c252ce2f9c26316fe3ffd4f2
Asgard09 <thanhphuc04nh@gmail.com>
2025-04-01 15:22:59 +0700

    UpdateUpdate

--- 13. Heap/_621_Task_Scheduler.java
+++ 13. Heap/_621_Task_Scheduler.java
@@ -1,20 +1,25 @@
 import java.util.Arrays;
-
+/*
+ * Step 1: Count the Frequency of Each Task
+ * Step 2: Calculate the Number of Chunks
+ * Step 3: Try to Fill the Idle Slots
+ */
 public class _621_Task_Scheduler {
     public static int leastInterval(char[] tasks, int n) {
         int[] freq = new int[26];
-        for (char task : tasks) {
+        for(char task : tasks){
             freq[task - 'A']++;
         }
-        Arrays.sort(freq);
-        int chunk = freq[25] - 1;
-        int idle = chunk * n;
 
-        for (int i = 24; i >= 0; i--) {
-            idle -= Math.min(chunk, freq[i]);
+        Arrays.sort(freq);
+        int chunks = freq[25] - 1;
+        int idles = n*chunks;
+
+        for(int i = 24; i>=0; i--){
+            idles -= Math.min(chunks, freq[i]);
         }
 
-        return idle < 0 ? tasks.length : tasks.length + idle;
+        return idles<0? tasks.length : tasks.length + idles;
     }
 
     public static void main(String[] args) {
