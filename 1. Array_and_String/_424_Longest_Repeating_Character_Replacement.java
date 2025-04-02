04088fa086b5bf0edcf9bd9f2f8ff8eb5e1f2c12
Asgard09 <thanhphuc04nh@gmail.com>
2025-04-02 11:22:32 +0700

    Longest Repeating Character Replacement

--- 1. Array_and_String/_424_Longest_Repeating_Character_Replacement.java
+++ 1. Array_and_String/_424_Longest_Repeating_Character_Replacement.java
@@ -1,2 +1,24 @@
+/*
+ * If a window size - most frequent > k, shrink the window
+ */
 public class _424_Longest_Repeating_Character_Replacement {
+    public int characterReplacement(String s, int k) {
+        int start  = 0, maxCount = 0;
+        int maxLength = 0;
+        int[] freq = new int[26];
+
+        for(int end = 0; end<s.length(); end++){
+            freq[s.charAt(end) - 'A'] ++;
+            maxCount = Math.max(maxCount, freq[s.charAt(end) - 'A']);
+
+            while ((end - start + 1) - maxCount > k) {
+                freq[s.charAt(start) - 'A']--;
+                start++;
+            }
+
+            maxLength = Math.max(maxLength, end - start + 1);
+        }
+
+        return maxLength;
+    }
 }
