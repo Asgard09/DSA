17a016ab7d1eccb08d2ca877a933c5fe3760444b
Asgard09 <thanhphuc04nh@gmail.com>
2025-04-07 10:36:35 +0700

    Update

--- 5. Binary Search/_875_Koko_Eating_Bananas.java
+++ 5. Binary Search/_875_Koko_Eating_Bananas.java
@@ -2,25 +2,25 @@ import java.util.Arrays;
 
 /*Create a separate class BinarySearch is not use all cases  */
 public class _875_Koko_Eating_Bananas {
-    public int minEatingSpeed(int[] piles, int h){
+    public int minEatingSpeed(int[] piles, int h) {
         int left = 1;
         int right = Arrays.stream(piles).max().getAsInt();
-        int res = 0;
+        int res = right;
 
-        while (left <= right) {
-            int mid = (left + right)/2;
+        while(left <= right){
+            int mid = (left+right)/2;
             int totalTime = 0;
 
-            for (int pile : piles) {
+            for(int pile: piles){
                 totalTime += Math.ceil((double)pile/mid);
             }
-
-            if (totalTime <= h) {
+            
+            if(totalTime <= h){
                 res = mid;
                 right = mid - 1;
-            }else {
+            }else{
                 left = mid + 1;
-            }
+            }      
         }
 
         return res;
