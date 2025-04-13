69c3f4e52610f4ef7de61af90d8b95d664adfbd2
Asgard09 <thanhphuc04nh@gmail.com>
2025-04-13 20:40:41 +0700

    Update

--- 8. LinkedList/_143_Reorder_List.java
+++ 8. LinkedList/_143_Reorder_List.java
@@ -3,34 +3,36 @@ public class _143_Reorder_List {
         if (head == null) return;
 
         // Step 1: Find the middle of the list
-        ListNode slow = head, fast = head;
-        while (fast != null && fast.next != null) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast!= null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
 
         // Step 2: Reverse the second half of the list
-        ListNode second = slow.next;
+        ListNode curr = slow.next;
+        ListNode second = null;
         slow.next = null;
-        ListNode node = null;
 
-        while (second != null) {
-            ListNode temp = second.next;
-            second.next = node;
-            node = second;
-            second = temp;
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = second;
+            second = curr;
+            curr = temp;
         }
 
         // Step 3: Merge the two halves
-        ListNode first = head;
-        second = node;
-
-        while (second != null) {
-            ListNode temp1 = first.next, temp2 = second.next;
-            first.next = second;
+        ListNode one = head;
+        
+        while (one != null && second != null) {
+            ListNode temp1 = one.next;
+            ListNode temp2 = second.next;
+            one.next = second;
             second.next = temp1;
-            first = temp1;
+            one = temp1;
             second = temp2;
-        }   
+        }
     }
 }
